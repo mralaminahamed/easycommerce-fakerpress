909b111916075daa6f84014b1b89309f264ee47f
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2025-09-15 09:25:19 +0600

    feat(validation): add validation endpoints for data availability and dependencies

--- class-easycommerce-fakerpress.php
+++ class-easycommerce-fakerpress.php
@@ -20,6 +20,7 @@ use EasyCommerceFakerPress\Controllers\Tax_REST_Controller;
 use EasyCommerceFakerPress\Controllers\Transaction_REST_Controller;
 use EasyCommerceFakerPress\Controllers\Cart_Session_REST_Controller;
 use EasyCommerceFakerPress\Controllers\Location_REST_Controller;
+use EasyCommerceFakerPress\Controllers\Validation_REST_Controller;
 
 /**
  * Main Plugin Class
@@ -220,6 +221,9 @@ class EasyCommerce_FakerPress {
 			new Transaction_REST_Controller(),
 			new Cart_Session_REST_Controller(),
 			new Location_REST_Controller(),
+
+			// Utility endpoints.
+			new Validation_REST_Controller(),
 		);
 
 		foreach ( $controllers as $controller ) {
