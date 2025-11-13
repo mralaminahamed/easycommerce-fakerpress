f9ed4bbf019d25530268f9da5ef7d562ba8141ca
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2025-11-13 16:55:42 +0600

    feat: bump version to 2.0.0 for major release

--- easycommerce-fakerpress.php
+++ easycommerce-fakerpress.php
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EASYCOMMERCE_FAKERPRESS_VERSION', '1.0.3' );
+define( 'EASYCOMMERCE_FAKERPRESS_VERSION', '2.0.0' );
 define( 'EASYCOMMERCE_FAKERPRESS_PLUGIN_FILE', __FILE__ );
 define( 'EASYCOMMERCE_FAKERPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'EASYCOMMERCE_FAKERPRESS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
