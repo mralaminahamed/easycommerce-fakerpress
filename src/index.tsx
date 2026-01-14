621ec59420211882c222382df51c82ff12b22155
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-01-14 09:56:54 +0600

    refactor: restructure frontend build entry point to src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import domReady from '@wordpress/dom-ready';
-import { createRoot } from '@wordpress/element';
 import React from 'react';
 
-import App from './components/App';
+import domReady from '@wordpress/dom-ready';
+import { createRoot } from '@wordpress/element';
+
+import App from '@/admin/components/App';
 import './styles.css';
 
 domReady( () => {
-	const container = document.getElementById( 'easycommerce-fakerpress-root' );
+	const container = document.getElementById( 'easycommerce-fakerpress-root' )!;
 	if ( container ) {
 		const root = createRoot( container );
 		root.render( <App /> );
